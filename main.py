ba39aa90bc680b4dcf3f598d90bf5aebe0150c93
pjmattingly <pjmattingly@users.noreply.github.com>
2022-03-28 20:55:48 -0600

    refactored DB into a class

--- main.py
+++ main.py
@@ -1,22 +1,41 @@
 _DEBUG = True
 
+class Database:
+    def __init__(self):
+        self._db = {}
+        self._db["_passwords"] = []
+
+    def add_password(self, password):
+        self._db["_passwords"].append( self._create_password(password) )
+
+    def _create_password(self, password):
+        #hash a plain-text password for storage
+        #see: https://werkzeug.palletsprojects.com/en/2.1.x/utils/#werkzeug.security.generate_password_hash
+        from werkzeug.security import generate_password_hash
+        return generate_password_hash(password)
+
+    def get_passwords(self):
+        return self._db["_passwords"]
+
+    def __repr__(self):
+        res = {}
+        #avoid exposing internal database keys (that start with _)
+        for key in self._db.keys():
+            if not str(key).startswith("_"):
+                res[key] = self._db[key]
+        
+        return res
+
 from flask import Flask
 app = Flask(__name__)
 
 from flask_restx import Api
 api = Api(app)
 
-db = {}
-db["_passwords"] = []
-
-def create_password(password):
-    #hash a plain-text password for storage
-    #see: https://werkzeug.palletsprojects.com/en/2.1.x/utils/#werkzeug.security.generate_password_hash
-    from werkzeug.security import generate_password_hash
-    return generate_password_hash(password)
+db = Database()
 
 #TEST
-db["_passwords"].append( create_password("test") )
+db.add_password("test")
 
 def check_for_access(request_form, passwords):
     from werkzeug.exceptions import Unauthorized
@@ -41,27 +60,23 @@ def check_for_event_format(request_form):
     if not "data" in request_form:
         raise BadRequest("Parameter 'data' is required.") #400
 
-def _repr_db(db):
-    #avoid exposing internal database keys (that start with _)
-    return {key:db[key] for key in db.keys() if not str(key).startswith("_") }
-
 from flask_restx import Resource
 from flask import request
 
 @api.route('/log')
 class Main(Resource):
     def post(self):
-        check_for_access(request.form, db["_passwords"])
+        check_for_access( request.form, db.get_passwords() )
         check_for_event_format(request.form)
         
         db.append( request.form['data'] )
 
-        return _repr_db(db)
+        return db.__repr__()
 
     def get(self):
-        check_for_access(request.form, db["_passwords"])
+        check_for_access(request.form, db.get_passwords() )
 
-        return _repr_db(db)
+        return db.__repr__()
 
 if __name__ == '__main__':
     app.run(debug=_DEBUG)
