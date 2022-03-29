79158f06d2fff16ead5d000b76120b74c837b47a
pjmattingly <pjmattingly@users.noreply.github.com>
2022-03-28 19:27:22 -0600

    Simple authentication started
    Checking for the "key" parameter in HTTP requests to check for authorization.

--- main.py
+++ main.py
@@ -6,18 +6,35 @@ app = Flask(__name__)
 from flask_restx import Api
 api = Api(app)
 
-db = []
+db = {}
+db["passwords"] = []
+
+def check_for_access(request_form, passwords):
+    if not "key" in request_form:
+        #instead of using the flask abort() use the internal exceptions
+        #see: https://flask.palletsprojects.com/en/2.1.x/errorhandling/
+        #https://werkzeug.palletsprojects.com/en/2.1.x/exceptions/
+        from werkzeug.exceptions import Unauthorized
+        raise Unauthorized("Parameter 'key' required.") #401
+    
+    #hash the key and check if it's authorized
+
 
 from flask_restx import Resource
+from flask import request
 
 @api.route('/log')
 class Main(Resource):
     def post(self):
-        from flask import request
+        check_for_access(request.form, db["passwords"])
+        
         db.append( request.form['data'] )
+
         return db
 
     def get(self):
+        check_for_access(request.form, db["passwords"])
+
         return db
 
 if __name__ == '__main__':
