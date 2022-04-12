f6f74a260b6bb35897ed460fbc81eba85a604a1b
pjmattingly <pjmattingly@users.noreply.github.com>
2022-04-11 19:34:04 -0600

    Added docstrings to validation and standardization

--- src/Validation_and_Standardization_Handler.py
+++ src/Validation_and_Standardization_Handler.py
@@ -1,23 +1,51 @@
+"""
+Validation and Standardization module
+
+This module contains the logic to validate input; As well as raising helpful
+errors back to the caller.
+
+In addition, this module handles appending sensible defaults for optional
+parameters of the input.
+"""
+
 '''
 TODO
 The validation portion of this module should be handled by Marshmallow
-but our needs are so simple at this time, that these simple functions should
+but our needs are so simple, at this time, that these simple functions should
 suffice.
     see:
     https://marshmallow.readthedocs.io/en/stable/quickstart.html
 '''
+
 #see: https://stackoverflow.com/questions/1319615/proper-way-to-declare-custom-exceptions-in-modern-python
 class Malformed_Input(Exception): pass
 
 def validate(row):
+    """
+    A function that acts as an entrypoint for the validation logic.
+
+    :param row: dictionary
+    """
     vns = Validation_and_Standardization()
     vns.validate(row)
 
 def standardize(row):
+    """
+    A function that acts as an entrypoint for adding sensible defaults to
+    the input.
+
+    :param row: dictionary
+    """
+
     vns = Validation_and_Standardization()
     return vns.standardize(row)
 
 class Validation_and_Standardization:
+    """
+    A class that wraps the logic of validation, as well as the code to
+    append sensible defaults to the input.
+    """
+
     def __init__(self):
         self._checks = [self._zero_length_check, self._required_keys_check]
         self._mods = [
@@ -36,9 +64,26 @@ class Validation_and_Standardization:
         }
 
     def validate(self, row):
+        """
+        A function that iterates over the various validation functions associated
+        with the class. The functions raises a `Malformed_Input` exception
+        when an issue occurs.
+
+        :param row: dictionary
+        """
+
         [func(row) for func in self._checks]
 
     def standardize(self, row):
+        """
+        A function that iterates over the various functions checking for optional
+        parameters and adding defaults. The row is modified iteratively
+        through each function call.
+
+        :param row: dictionary
+        :return: dictionary
+        """
+
         _row = row
         for func in self._mods:
             _row = func(_row)
@@ -65,6 +110,14 @@ class Validation_and_Standardization:
         return row
 
     def _required_keys_check(self, row):
+        """
+        A function to check a row of input for the required keys needed for
+        inserting a new row of data.
+
+        :param row: dictionary
+        :return: dictionary
+        """
+
         _msg = self._error_msgs["_required_keys_check"]
 
         seen_keys = set( row.keys() )
@@ -75,12 +128,28 @@ class Validation_and_Standardization:
         return row
 
     def _private_keys_check(self, row):
+        """
+        A function to check a row of input for keys that begin with an underscore
+        and raising on finding them; As keys beginning with an underscore
+        are reserved for internal use.
+
+        :param row: dictionary
+        """
+
         _msg = self._error_msgs["_private_keys_check"]
          
         if any( [str(k).startswith("_") for k in row.keys()] ):
             raise Malformed_Input(_msg)
 
     def _date_time_check(self, row):
+        """
+        A function to check for the "date_time" parameter in input, and appending
+        a sensible default timestamp to the input if "date_time" is not present.
+
+        :param row: dictionary
+        :return: dictionary
+        """
+
         #TODO, we should check if date_time is valid here, but this is very complex
         if (not "date_time" in list(row)):
             from datetime import datetime
@@ -91,6 +160,15 @@ class Validation_and_Standardization:
         return row
 
     def _notes_check(self, row):
+        """
+        A function to check for the "notes" parameter in input, and appending
+        a sensible default value assocaited with a "notes" parameter
+        if "notes" is not present.
+
+        :param row: dictionary
+        :return: dictionary
+        """
+
         if (not "notes" in list(row)):
             _row = dict(row)
             _row["notes"] = None
@@ -98,6 +176,13 @@ class Validation_and_Standardization:
         return row
 
     def _add_timestamp(self, row):
+        """
+        A function to append a timestamp to each input.
+
+        :param row: dictionary
+        :return: dictionary
+        """
+
         _row = dict(row)
         from datetime import datetime
         _row["_timestamp"] = datetime.now(tz=None)
